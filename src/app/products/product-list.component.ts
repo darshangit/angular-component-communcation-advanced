9c70c3aa03999d582d068325fa664253a928fabf
darshangit <darshans1989@gmail.com>
2018-09-06 20:39:55 +0530

    View Childrem

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, AfterViewInit {
+
+    pageTitle: string = 'Product List';
+    showImage: boolean;
+
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    errorMessage: string;
+
+    filteredProducts: IProduct[];
+    products: IProduct[];
+
+    @ViewChild('filterElement') filterElementRef: ElementRef;
+
+    private _listFilter: string;
+
+    constructor(private productService: ProductService) {
+     }
+
+     ngAfterViewInit(): void {
+        this.filterElementRef.nativeElement.focus();
+    }
+
+
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.performFilter(this._listFilter);
+    }
+
+
+    ngOnInit(): void {
+        this.productService.getProducts().subscribe(
+            (products: IProduct[]) => {
+                this.products = products;
+                this.performFilter(this.listFilter);
+            },
+            (error: any) => this.errorMessage = <any>error
+        );
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    performFilter(filterBy?: string): void {
+        if (filterBy) {
+            this.filteredProducts = this.products.filter((product: IProduct) =>
+                product.productName.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase()) !== -1);
+        } else {
+            this.filteredProducts = this.products;
+        }
+    }
+}
